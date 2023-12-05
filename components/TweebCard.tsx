61e6a0dd805e54f5e9a4eab0073ae799f157fff7
etiopia01 <etiopiasalvatore@gmail.com>
2023-12-05 02:06:28 +0100

    ready for test deploy

--- components/TweebCard.tsx
+++ components/TweebCard.tsx
@@ -6,6 +6,7 @@ export default function TweebCard ({tweeb, remove}: {tweeb: Tweeb, remove:(id:nu
     
     const session = useSession()
     const supabase = useSupabaseClient()
+   
 
     const handleDelete = async ()=> {
         const { data, error } = await supabase
@@ -17,10 +18,13 @@ export default function TweebCard ({tweeb, remove}: {tweeb: Tweeb, remove:(id:nu
         remove(tweeb.id)
     }
     return (
-        <div className="w-auto flex flex-col justify-between items-start bg-slate-800 rounded-md p-6">
+        <div className=" w-auto flex flex-col justify-between  items-start bg-slate-800 rounded-md py-2 px-6">
+            <p className="text-xs  text-slate-500 mb-4">{tweeb.author} <span>writes</span></p>
+            <div className="flex flex-col gap-2">
             <h1 className="text-2xl  text-slate-200">{tweeb.title}</h1>
             <h3 className="text-md  text-slate-500">{tweeb.topic}</h3>
             <p className="text-lg  text-slate-200">{tweeb.content}</p>
+            </div>
             {tweeb.author === session?.user.user_metadata.name && <button className="p-1 rounded-lg bg-slate-600 ml-auto" onClick={handleDelete}>X</button>}
         </div>
     )
