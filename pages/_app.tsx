407f7e3fb490b9885b01397f3fc60d43cb675f61
etiopia01 <etiopiasalvatore@gmail.com>
2023-12-05 01:35:36 +0100

    env bugs probably fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const supabaseClient = createPagesBrowserClient()
+  const supabaseClient = createPagesBrowserClient({supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrdXFkbHJ1c2lqZXVhZWxibHdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1MjA0MDIsImV4cCI6MjAxNzA5NjQwMn0.-nbX3r2qRBAfkKJtyd4YZZoPbVY_H0X3NIRdVCGYqnY", supabaseUrl:"https://dkuqdlrusijeuaelblwr.supabase.co"})
   return (
     <SessionContextProvider supabaseClient={supabaseClient}
     initialSession={pageProps.initialSession}>
